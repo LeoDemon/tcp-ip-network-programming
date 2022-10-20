5abf4f8473cf0c90f7c411352855fc5d37321c43
DemonLee <jwkljh@aliyun.com>
2022-10-20 09:27:14 +0800

    ch10 coding examples: grandchildren process recycle

--- ch10/remove_zombie.c
+++ ch10/remove_zombie.c
@@ -8,9 +8,50 @@
 #include <signal.h>
 #include <stdlib.h>
 
+void recycle_child(int signal) {
+    printf("receive signal: %d\n", signal);
+    int status;
+    pid_t child_pid;
+    while ((child_pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        printf("pid-%d recycle child: %d, state: %d\n", getpid(), child_pid, WEXITSTATUS(status));
+    }
+}
+
+void register_signal() {
+    struct sigaction act;
+    act.__sigaction_u.__sa_handler = recycle_child;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGCHLD, &act, 0);
+}
+
+void register_signal_default() {
+    struct sigaction act;
+    act.__sigaction_u.__sa_handler = NULL;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGCHLD, &act, 0);
+}
+
 int child(int k) {
     printf("I am child-%d: %d\n", k, getpid());
-    sleep(10 + k);
+
+    if (k == 3) {
+        pid_t child_pid = fork();
+        if (0 == child_pid) {
+            // 由于子进程继承了父进程的全部能力，故默认情况下，子进程也有 recycle_child 的能力，因而该孙子进程能被子进程回收
+            child(100);
+        } else {
+            register_signal_default(); // 屏蔽该子进程的回收能力
+            printf("child-%d create a new child: %d\n", getpid(), child_pid);
+        }
+    }
+
+    if (k == 100) {
+        sleep(100);
+    } else {
+        sleep(60 + k);
+    }
     printf("child-%d: %d exit now\n", k, getpid());
     exit(k);
 }
@@ -26,22 +67,8 @@ void parent(int flag) {
     }
 }
 
-void recycle_child(int signal) {
-    printf("receive signal: %d\n", signal);
-    int status;
-    pid_t child_pid;
-    while ((child_pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        printf("recycle child: %d, state: %d\n", child_pid, WEXITSTATUS(status));
-    }
-}
-
 int main() {
-    struct sigaction act;
-    act.__sigaction_u.__sa_handler = recycle_child;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
-    // comment this line for testing zombie process recycle
-    sigaction(SIGCHLD, &act, 0);
+    register_signal();
 
     pid_t pid;
     for (int i = 0; i < 5; i++) {
